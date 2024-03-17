5e8739ac6b3c9cd98f06a6fb1e3261815f5999dd
Andy Feetenby <andyfeetenby@gmail.com>
2024-03-17 18:57:11 +0700

    Created Custom Hook, replacing UseState, UseEffect in Board Component

--- src/pages/Board/Board.js
+++ src/pages/Board/Board.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import useDataFetching from '../../hooks/useDataFetching'; 
 import Lane from '../../components/Lane/Lane';
 import './Board.css';
 
@@ -10,30 +10,10 @@ const lanes = [
 ];
 
 function Board() {
-  const [ loading, setLoading ] = useState(false);
-  const [ tasks, setTasks ] = useState([]);
-  const [ error, setError ] = useState('');
-
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const tasks = await fetch(
-          `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`, 
-        );
-
-        const result = await tasks.json();
-        if(result){
-          setTasks(result);
-          setLoading(false);
-        }
-      }
-      catch(e){
-        setLoading(false);
-        setError(e.message);
-      }
-    }
-    fetchData();
-  }, []);
+  const [ loading, error, tasks ] = useDataFetching(
+    `https://my-json-server.typicode.com/PacktPublishing/React-Projects-Second-Edition/tasks`
+  );
+  
   return (
     <div className='Board-wrapper'>
       {lanes.map((lane) => (
