72d4f67def6fab38c2cfc1d389f45b8344b6b8f9
Andy Feetenby <AndyFeetenby@gmail.com>
2024-03-18 11:00:19 +0700

    Style Components

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -1,11 +1,23 @@
-import './Task.css';
+import styled from "styled-components";
+
+const TaskWrapper = styled.div`
+    background: darkgray;
+    padding: 20px;
+    border-radius: 20px;
+    margin: 0% 5% 5% 5%;
+`
+
+const Title = styled.h3`
+    width: 100%;
+    margin: 0;
+`
 
 function Task({ id, title, body, onDragStart }){
     return(
-        <div className='Task-wrapper' draggable onDragStart = {(e) => onDragStart(e, id)}>
-            <h3>{ title }</h3>
+        <TaskWrapper draggable onDragStart = {(e) => onDragStart(e, id)}>
+            <Title>{ title }</Title>
             <p>{ body }</p>
-        </div>
+        </TaskWrapper>
     );
 }
 
